241256dd390dcce30957c2c667b00ac0dd212c81
Daniel <danielmichael.fabricius@gmail.com>
2022-11-25 02:54:36 +0100

    Implemented console applicaion

---
+++ ShoppingList.Database/ShoppingListContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Database
+{
+    public class ShoppingListContextFactory
+    {
+        public static ShoppingListContext CreateInMemoryDbContext()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            var builder = new DbContextOptionsBuilder<ShoppingListContext>();
+            builder.UseSqlite(connection);
+
+            var context = new ShoppingListContext(builder.Options);
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+}
--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ShoppingList.Database;
+using ShoppingList.Core;
+
+var context = ShoppingListContextFactory.CreateInMemoryDbContext();
+var repository = new ProductRepository(context);
+
+while (true)
+{
+    try
+    {
+        string input = Console.ReadLine()!;
+        var splitInput = input.Split(' ');
+
+        switch (splitInput[0].ToLower())
+        {
+            case "add":
+                var product = new ProductDTO(splitInput[1], double.Parse(splitInput[2]));
+                repository.Add(product);
+                break;
+
+            case "delete":
+                repository.Delete(splitInput[1]);
+                break;
+
+            case "list":
+                var products = repository.GetAll();
+                var print = products.Select(p => p.ToString()).Aggregate((l1, l2) => $"{l1}\n{l2}");
+                Console.WriteLine("\n" + print + "\n");
+                break;
+            default:
+                throw new Exception();
+        }
+    } 
+    catch(Exception) 
+    {
+        Console.WriteLine("Invaild command");
+    }
+}
+
+
