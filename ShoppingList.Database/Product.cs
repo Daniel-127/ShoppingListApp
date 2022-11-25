75ac8f85b2d5ef926b6003343968b44353a4aae0
Daniel <danielmichael.fabricius@gmail.com>
2022-11-25 01:09:41 +0100

    Implemented product entity and dbcontext

---
+++ ShoppingList.Database/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Database
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required double Price { get; set; }
+    }
+}
---
+++ ShoppingList.Database/ShoppingListContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Database
+{
+    public class ShoppingListContext : DbContext
+    {
+        public DbSet<Product> Products => Set<Product>();
+        public ShoppingListContext(DbContextOptions<ShoppingListContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
